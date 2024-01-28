af35a3c2590a10c9d7d6a90e4886b3379ae118a5
zenbooster <zenbooster@gmail.com>
2024-01-28 19:45:15 +0300

    Сделал длительность вибрации 333 мс, максимальный уровень.

--- app/src/main/java/com/example/phasig/MyService.kt
+++ app/src/main/java/com/example/phasig/MyService.kt
@@ -8,15 +8,9 @@ import android.app.PendingIntent
 import android.content.Intent
 import android.os.IBinder
 import android.os.Build
-import android.os.Looper
-import android.os.Handler
-import android.os.Message
-import android.os.HandlerThread
-import android.os.Process
 import android.widget.Toast
 import android.os.Vibrator
 import android.os.VibrationEffect;
-//import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -26,6 +20,7 @@ import android.graphics.Color
 import com.example.phasig.presentation.MainActivity
 import android.app.PendingIntent.FLAG_IMMUTABLE
 import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_HEALTH
 
 class MyService : Service(), SensorEventListener {
     private var mSensorManager : SensorManager ?= null
@@ -51,7 +46,7 @@ class MyService : Service(), SensorEventListener {
                 val vibrator = getSystemService(VIBRATOR_SERVICE) as Vibrator
                 val vibrationEffect1: VibrationEffect
                 vibrationEffect1 =
-                    VibrationEffect.createOneShot(1000, VibrationEffect.DEFAULT_AMPLITUDE)
+                    VibrationEffect.createOneShot(333, 255) //VibrationEffect.DEFAULT_AMPLITUDE)
 
                 // it is safe to cancel other vibrations currently taking place
                 vibrator.cancel()
@@ -112,24 +107,12 @@ class MyService : Service(), SensorEventListener {
     override fun onDestroy()
     {
         mSensorManager!!.unregisterListener(this)
-        //super.onDestroy()
     }
 
     override fun onCreate() {
-        // Start up the thread running the service.  Note that we create a
-        // separate thread because the service normally runs in the process's
-        // main thread, which we don't want to block.  We also make it
-        // background priority so CPU-intensive work will not disrupt our UI.
-        /*HandlerThread("ServiceStartArguments", Process.THREAD_PRIORITY_BACKGROUND).apply {
-            start()
-
-            // Get the HandlerThread's Looper and use it for our Handler
-            //serviceLooper = looper
-            //serviceHandler = ServiceHandler(looper)
-        }*/
         mSensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
         mAccelerometer = mSensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-        startForeground(1, createNotification())
+        startForeground(FOREGROUND_SERVICE_TYPE_HEALTH, createNotification())
     }
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
@@ -142,13 +125,6 @@ class MyService : Service(), SensorEventListener {
 
         mSensorManager!!.registerListener(this,mAccelerometer,
             SensorManager.SENSOR_DELAY_GAME)
-
-        // For each start request, send a message to start a job and deliver the
-        // start ID so we know which request we're stopping when we finish the job
-        /*serviceHandler?.obtainMessage()?.also { msg ->
-            msg.arg1 = startId
-            serviceHandler?.sendMessage(msg)
-        }*/
 
         // If we get killed, after returning from here, restart
         return START_NOT_STICKY
